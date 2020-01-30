d8d8590f955770ad6e9f590821a7f6e876ef5ea5
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-30 15:42:38 +0530

    ADDED:feature of Snake,Ladder or No-play in the game

--- snakeAndLadderSimulator2.sh
+++ snakeAndLadderSimulator2.sh
@@ -1,4 +1,30 @@
 #!/bin/bash -x
 echo "-------------------------------------------WELLCOME TO SNAKE AND LADDER SIMULATOR---------------------------------------------"
+NO_PLAY=0;
+SNAKE=1;
+LADDER=2;
+
 playerStartPosition=0
-die=$((RANDOM%6+1))
+
+function rollDie(){
+	local die=$((RANDOM%6+1))
+	echo $die
+}
+
+function checkNoPlaySnakeOrLadder(){
+	local playerPosition=$2
+	case $((RANDOM%3)) in
+		$NO_PLAY)
+			playerPosition=$playerPosition
+			;;
+		$SNAKE)
+			playerPosition=$(($playerPosition-$1))
+			;;
+		$LADDER)
+			playerPosition=$(($playerPosition+$1))
+			;;
+	esac
+	echo $playerPosition
+}
+
+playerStartPosition=$(checkNoPlaySnakeOrLadder $(rollDie) $playerStartPosition )
