8d30f0ee3cf03b63aeadb1cd68bc2bce3cf6dbca
Your Name <avatarmalekar4@gmail.com>
2020-01-31 18:05:51 +0530

    ADDED:compatiblity for two players to play

--- snakeAndLadderSimulator2.sh
+++ snakeAndLadderSimulator2.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 echo "-------------------------------------------WELLCOME TO SNAKE AND LADDER SIMULATOR---------------------------------------------"
+
+#CONSTANT
 NO_PLAY=0;
 SNAKE=1;
 LADDER=2;
 WINNING_POSITION=100;
 PLAYER_START_POSITION=0;
+PLAYER1=0
+PLAYER2=1
 
-playerPosition=$PLAYER_START_POSITION
-numberOfTimeDiceTossed=0
-flag=0
+#VARIABLES
+player1Position=$PLAYER_START_POSITION
+player2Position=$PLAYER_START_POSITION
+playingTurn=$PLAYER1
+numberOfTimeDiceTossedByPlayer1=0
+numberOfTimeDiceTossedByPlayer2=0
+flagForPlayer1=0
+flagForPlayer2=0
+
+#FUNCTIONS
 function rollDie(){
 	local die=$((RANDOM%6+1))
 	echo $die
 }
-
 function checkNoPlaySnakeOrLadder(){
 	local playerTempPosition=$2
 	case $((RANDOM%3)) in
@@ -30,7 +40,6 @@ function checkNoPlaySnakeOrLadder(){
 	playerTempPosition=$(getExactWinningPosition $playerTempPosition $1 )
 	echo $playerTempPosition
 }
-
 function checkPositionBelowZero(){
 	local position=$1
 	if [ $position -lt 0 ]
@@ -57,20 +66,43 @@ function winnerDecider(){
 	echo $flagValue
 }
 
-function diceTossed(){
-	((numberOfTimeDiceTossed++))
+function diceTossedByPlayer1(){
+	((numberOfTimeDiceTossedByPlayer1++))
+}
+function diceTossedByPlayer2(){
+	((numberOfTimeDiceTossedByPlayer2++))
 }
 
 function positionAfterEveryDieForPlayer(){
-	echo "Position is:"$1
+	echo " " $2" is:"$1
 }
-while [ $flag -eq 0 ]
+function getWinner(){
+	if [ $1 -gt $2 ]
+	then
+		echo "Player-1 is Winner..!!"
+	else
+		echo "Player-2 is Winner..!!"
+	fi
+}
+
+#MAIN
+while [ $flagForPlayer1 -eq 0 -a $flagForPlayer2 -eq 0 ]
 do
-	playerPosition=$(checkNoPlaySnakeOrLadder $(rollDie) $playerPosition )
-	playerPosition=$(checkPositionBelowZero $playerPosition )
-	positionAfterEveryDieForPlayer $playerPosition
-	flag=$(winnerDecider $playerPosition )
-	diceTossed
+	if [ $playingTurn -eq  $PLAYER1 ]
+	then
+		player1Position=$(checkNoPlaySnakeOrLadder $(rollDie) $player1Position )
+		player1Position=$(checkPositionBelowZero $player1Position )
+		positionAfterEveryDieForPlayer $player1Position $"player1Position"
+		flagForPlayer1=$(winnerDecider $player1Position )
+		diceTossedByPlayer1
+		playingTurn=$PLAYER2
+	else
+		player2Position=$(checkNoPlaySnakeOrLadder $(rollDie) $player2Position )
+		player2Position=$(checkPositionBelowZero $player2Position )
+		positionAfterEveryDieForPlayer $player2Position $"player2Position"
+		flagForPlayer2=$(winnerDecider $player2Position )
+		diceTossedByPlayer2
+		playingTurn=$PLAYER1
+	fi
 done
-echo "Final Position of Player:"$playerPosition
-echo "Number of times dice tossed:"$numberOfTimeDiceTossed
+getWinner $flagForPlayer1 $flagForPlayer2
