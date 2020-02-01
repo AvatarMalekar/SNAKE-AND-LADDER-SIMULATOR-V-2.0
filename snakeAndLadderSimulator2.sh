c4413453ae4a6e59d4b60c2cb49ee64924138130
Your Name <avatarmalekar4@gmail.com>
2020-02-01 12:56:45 +0530

    MODIFIED:proper naming conventions followed for variables

--- snakeAndLadderSimulator2.sh
+++ snakeAndLadderSimulator2.sh
@@ -17,16 +17,18 @@ playerDictionary[Player2]=$PLAYER_START_POSITION
 numberOfTimeDiceTossed=0
 
 #THESE VARIABLES ARE USED FOR SWITCHING PLAYERS 
-i=1
-x=3
+playerSwitch=$PLAYER2
+playerSwitchingLimit=3
 
 #FUNCTIONS
 function rollDie(){
 	echo $((RANDOM%6+1))
 }
+
 function diceTossed(){
 	((numberOfTimeDiceTossed++))
 }
+
 function checkNoPlaySnakeOrLadder(){
 	local playerCurrentPosition=$2
 	local dieValue=$1
@@ -48,11 +50,13 @@ function checkNoPlaySnakeOrLadder(){
 	playerCurrentPosition=$(checkPositionBelowZero $playerCurrentPosition )
 	echo $playerCurrentPosition
 }
+
 function positionAfterEveryDieForPlayer(){
 	local position=$1
 	local player=$2
 	echo "position of "$player "is" $position
 }
+
 function checkPositionBelowZero(){
 	local position=$1
 	if [ $position -lt 0 ]
@@ -61,6 +65,7 @@ function checkPositionBelowZero(){
 	fi
 	echo $position
 }
+
 function getExactWinningPosition(){
 	local position=$1
 	local dieValue=$2
@@ -70,6 +75,7 @@ function getExactWinningPosition(){
 	fi
 	echo $position
 }
+
 function getWinner(){
 	local position=$1
 	local player=$2
@@ -82,11 +88,11 @@ function getWinner(){
 #MAIN
 while [ ${playerDictionary[Player1]} -ne $WINNING_POSITION -a ${playerDictionary[Player2]} -ne $WINNING_POSITION ]
 do
-	playerDictionary[Player"$i"]=$(checkNoPlaySnakeOrLadder $(rollDie) ${playerDictionary[Player"$i"]} )
-	positionAfterEveryDieForPlayer ${playerDictionary[Player"$i"]} $"PLayer-$i"
+	playerDictionary[Player"$playerSwitch"]=$(checkNoPlaySnakeOrLadder $(rollDie) ${playerDictionary[Player"$playerSwitch"]} )
+	positionAfterEveryDieForPlayer ${playerDictionary[Player"$playerSwitch"]} $"PLayer-$playerSwitch"
 	diceTossed
-	getWinner ${playerDictionary[Player"$i"]} $"PLayer-$i" 
-	i=$(($x-$i)) #PLAYER SWITCHES HERE
+	getWinner ${playerDictionary[Player"$playerSwitch"]} $"PLayer-$playerSwitch" 
+	playerSwitch=$(($playerSwitchingLimit-$playerSwitch)) #PLAYER SWITCHES HERE
 done
 
 echo "Total number of count of Dice tossed:-"$numberOfTimeDiceTossed
